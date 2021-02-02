691da020d311a3ae9bca5a9e8e50ad6d8b60d3c2
Marble <mrivera0014@gmail.com>
2021-02-02 00:14:45 -0700

    ajax call created and functioning, html modified to see where elements will be

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,23 @@
 //cities already declared
-var cities = ["Austin", "Chicago", "New York", "Orlando", "San Francisco", "Seattle", "Denver", "Atlanta"]
+var cities = ["Denver"]
+var APIKey = "deefa433382a6d1f06e77bd7935cf556"
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cities + "&appid=" + APIKey;
+
+$(document).ready(function () {
+    $("#search").on("click", function () {
+        // var addcity = 
+    })
+
+
+
+
+})
+
+
 
 // function that will create the buttons for the cities array
 function addButtons() {
-    // console.log("new cites added")
+    console.log("new cites added")
     $("#newCity").empty()
     for (var i = 0; i < cities.length; i++) {
 
@@ -12,11 +26,31 @@ function addButtons() {
         cityButton.text(cities[i])
 
         $("#newCity").append(cityButton)
-
     }
-
-
 }
+
+//ajax will retrieve the data from the API
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(queryURL)
+
+    //once data has been retrieved data will be placed into corresponding elements
+    //used classes from HTML to add data
+    $(".cityName").text(response.name);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".wind").text("Wind Speed: " + response.wind.speed)
+    // $(".uvIndex").text("UV Index: " + response.)
+
+    var temperature = (response.main.temp - 273.15) * 1.80 + 32;
+    $(".temp").text("Temperature(K): " + response.main.speed);
+    $(".temp").text("Temperature(F): " + temperature.toFixed(2));
+
+
+})
+
+
 
 
 
